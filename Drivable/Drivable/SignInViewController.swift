48899759b689c3cb09672b8081d274023aa9335d
BrandynM <brandyn.roatch@hotmail.com>
2022-07-15 22:14:50 -0700

    Finished layout for the Signup page, worked on keyboard for when entering details on the lower side of the page, and tested the connection to firebase before creating more accounts in iOS.

--- Drivable/Drivable/SignInViewController.swift
+++ Drivable/Drivable/SignInViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class SignInViewController: UIViewController, UITextFieldDelegate {
     
@@ -23,6 +26,7 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    //Firebase Variables
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +36,21 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         
         emailTF.delegate = self
         passwordTF.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
+    
+    //MARK: Actions
+    @IBAction func signInPressed(_ sender: UIButton) {
+        
+        CheckText()
+        
+    }
+    
 
-    //MARK: Methods
+    //MARK: Methods for Layout
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
@@ -50,6 +66,76 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         
         signInBtn.layer.cornerRadius = 10
         
+    }
+    
+    //MARK: Methods for Backend
+    
+    func CheckText() {
+        
+        if emailTF.text?.isEmpty == true && passwordTF.text?.isEmpty == true {
+            
+            //Both Email and Pasword are empty
+            let emptyAlert = UIAlertController(title: "Sorry...", message: "All required fields needed to login: Email and Password show empty.", preferredStyle: .alert)
+            
+            emptyAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(emptyAlert, animated: true, completion: nil)
+            
+        }
+        
+        else if emailTF.text?.isEmpty == false && passwordTF.text?.isEmpty == true {
+            
+            //emailTF is filled, but passwordTF is empty
+            let emptyAlert = UIAlertController(title: "Sorry...", message: "All required fields needed to login: Password show empty.", preferredStyle: .alert)
+            
+            emptyAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(emptyAlert, animated: true, completion: nil)
+            
+        }
+        
+        else if emailTF.text?.isEmpty == true && passwordTF.text?.isEmpty == false {
+            
+            //passwordTF is filled, but emailTF is empty
+            // -> in this case, make sure to empty the passwordTF
+            let emptyAlert = UIAlertController(title: "Sorry...", message: "All required fields needed to login: Email show empty.", preferredStyle: .alert)
+            
+            emptyAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(emptyAlert, animated: true, completion: nil)
+            
+            passwordTF.text?.removeAll()
+            
+        }
+        else {
+            
+            let email = emailTF.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTF.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            //both should be filled
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if error != nil{
+                    print(error?.localizedDescription)
+                }
+                else{
+                    print("Signed in Successful")
+                }
+            }
+            
+        }
+        
+    }
+    
+    //MARK: Methods for handling Keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
 }
--- Drivable/Drivable/SignupViewController.swift
+++ Drivable/Drivable/SignupViewController.swift
@@ -10,15 +10,63 @@ import UIKit
 class SignupViewController: UIViewController {
 
     //Views
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var elementsView: UIView!
+    @IBOutlet weak var inputTextView: UIView!
     
+    //Text Fields
+    @IBOutlet weak var companyTF: UITextField!
+    @IBOutlet weak var acronymTF: UITextField!
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var confirmTF: UITextField!
+    
+    //Buttons
+    @IBOutlet weak var signupBtn: UIButton!
+    @IBOutlet weak var addImageBtn: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setCorners()
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //MARK: Methods
+    func setCorners(){
+        
+        for view in [elementsView, inputTextView, scrollView]{
+            
+            view?.layer.cornerRadius = 20
+            
+        }
+        
+        signupBtn.layer.cornerRadius = 10
     }
 
+    
+    //MARK: Methods for handling Keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
 
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
 }
 
 
